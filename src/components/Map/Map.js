0118d26c18c15fd4c984e00189f4fefbb982898c
Cesar Benavides <cesarbenavides@icloud.com>
2020-12-16 08:16:52 -0600

    Reformatted views

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -22,35 +22,35 @@ const Map = ({ title, center, zoom }) => {
     const [locations, setLocations] = React.useState(example.response.locations)
     const [submitting, setSubmitting] = useState(false);
     const [searchCenter, setCenter] = useState({ lat: 29.4241, lng: -98.4936})
-    const [hoverRef, isHovered] = useHover();
+    const [hover, setHover] = useState(false)
 
-    // Hook
-    function useHover() {
-        const [value, setValue] = useState(false);
+    // // Hook
+    // function useHover() {
+    //     const [value, setValue] = useState(false);
 
-        const ref = useRef(null);
+    //     const ref = useRef(null);
 
-        const handleMouseOver = () => setValue(true);
-        const handleMouseOut = () => setValue(false);
+    //     const handleMouseOver = () => setValue(true);
+    //     const handleMouseOut = () => setValue(false);
 
-        useEffect(
-            () => {
-                const node = ref.current;
-                if (node) {
-                    node.addEventListener('mouseover', handleMouseOver);
-                    node.addEventListener('mouseout', handleMouseOut);
+    //     useEffect(
+    //         () => {
+    //             const node = ref.current;
+    //             if (node) {
+    //                 node.addEventListener('mouseover', handleMouseOver);
+    //                 node.addEventListener('mouseout', handleMouseOut);
 
-                    return () => {
-                        node.removeEventListener('mouseover', handleMouseOver);
-                        node.removeEventListener('mouseout', handleMouseOut);
-                    };
-                }
-            },
-            [] // Recall only if ref changes
-        );
+    //                 return () => {
+    //                     node.removeEventListener('mouseover', handleMouseOver);
+    //                     node.removeEventListener('mouseout', handleMouseOut);
+    //                 };
+    //             }
+    //         },
+    //         [] // Recall only if ref changes
+    //     );
 
-        return [ref, value];
-    }
+    //     return [ref, value];
+    // }
 
     // InfoWindow component
     const InfoWindow = (props) => {
@@ -119,8 +119,9 @@ const Map = ({ title, center, zoom }) => {
                             text={place.institutionName}
                             lat={place.latitude}
                             lng={place.longitude}
-                        >
-                                {isHovered && <InfoWindow place={place} />}
+                            show={true}
+                        >       
+                                {/* {hover && <InfoWindow place={place} />} */}
                                 <Pin className="pin" />
                         </Marker>
                     ))}
@@ -131,7 +132,7 @@ const Map = ({ title, center, zoom }) => {
 
 
 
-    const getLocations = ( request ) => {
+    const getLocations = async ( request ) => {
         setSubmitting(true)
         const proxy = 'https://cors-anywhere.herokuapp.com/'
 
@@ -158,10 +159,6 @@ const Map = ({ title, center, zoom }) => {
     }
 
 
-    // console.log(exampleResponse)
-    console.log(hoverRef)
-
-
     const Form = () => {
 
         const handleOnSubmit = (event) => {
@@ -181,100 +178,102 @@ const Map = ({ title, center, zoom }) => {
         return (
             <section className='form-wrapper'>
                 <form className='form' onSubmit={handleOnSubmit}>
-                    <input
-                        className='input address-input'
-                        type="text"
-                        name="address"
-                        placeholder="Address"
-                        required
-                    ></input>
-                    <input
-                        className='input city-input'
-                        type="text"
-                        name="city"
-                        placeholder="City"
-                        required
-                    ></input>
-                    <select
-                        name="state"
-                        required
-                        placeholder="State"
-                        className='input state'
-                    >
-                        <option value="Select" defaultValue="TX" disabled="">
-                            State
+                    <section className="checkboxes">
+                        <input
+                            className='input address-input'
+                            type="text"
+                            name="address"
+                            placeholder="Address"
+                            required
+                        ></input>
+                        <input
+                            className='input city-input'
+                            type="text"
+                            name="city"
+                            placeholder="City"
+                            required
+                        ></input>
+                    </section>
+                    <section className="checkboxes">
+                        <select
+                            name="state"
+                            required
+                            placeholder="State"
+                            className='input state'
+                        >
+                            <option value="Select" defaultValue="TX" disabled="">
+                                State
               </option>
-                        <option value="TX">Texas</option>
-                        <option value="AL">Alabama</option>
-                        <option value="AK">Alaska</option>
-                        <option value="AZ">Arizona</option>
-                        <option value="AR">Arkansas</option>
-                        <option value="CA">California</option>
-                        <option value="CO">Colorado</option>
-                        <option value="CT">Connecticut</option>
-                        <option value="DE">Delaware</option>
-                        <option value="DC">District Of Columbia</option>
-                        <option value="FL">Florida</option>
-                        <option value="GA">Georgia</option>
-                        <option value="HI">Hawaii</option>
-                        <option value="ID">Idaho</option>
-                        <option value="IL">Illinois</option>
-                        <option value="IN">Indiana</option>
-                        <option value="IA">Iowa</option>
-                        <option value="KS">Kansas</option>
-                        <option value="KY">Kentucky</option>
-                        <option value="LA">Louisiana</option>
-                        <option value="ME">Maine</option>
-                        <option value="MD">Maryland</option>
-                        <option value="MA">Massachusetts</option>
-                        <option value="MI">Michigan</option>
-                        <option value="MN">Minnesota</option>
-                        <option value="MS">Mississippi</option>
-                        <option value="MO">Missouri</option>
-                        <option value="MT">Montana</option>
-                        <option value="NE">Nebraska</option>
-                        <option value="NV">Nevada</option>
-                        <option value="NH">New Hampshire</option>
-                        <option value="NJ">New Jersey</option>
-                        <option value="NM">New Mexico</option>
-                        <option value="NY">New York</option>
-                        <option value="NC">North Carolina</option>
-                        <option value="ND">North Dakota</option>
-                        <option value="OH">Ohio</option>
-                        <option value="OK">Oklahoma</option>
-                        <option value="OR">Oregon</option>
-                        <option value="PA">Pennsylvania</option>
-                        <option value="RI">Rhode Island</option>
-                        <option value="SC">South Carolina</option>
-                        <option value="SD">South Dakota</option>
-                        <option value="TN">Tennessee</option>
+                            <option value="TX">Texas</option>
+                            <option value="AL">Alabama</option>
+                            <option value="AK">Alaska</option>
+                            <option value="AZ">Arizona</option>
+                            <option value="AR">Arkansas</option>
+                            <option value="CA">California</option>
+                            <option value="CO">Colorado</option>
+                            <option value="CT">Connecticut</option>
+                            <option value="DE">Delaware</option>
+                            <option value="DC">District Of Columbia</option>
+                            <option value="FL">Florida</option>
+                            <option value="GA">Georgia</option>
+                            <option value="HI">Hawaii</option>
+                            <option value="ID">Idaho</option>
+                            <option value="IL">Illinois</option>
+                            <option value="IN">Indiana</option>
+                            <option value="IA">Iowa</option>
+                            <option value="KS">Kansas</option>
+                            <option value="KY">Kentucky</option>
+                            <option value="LA">Louisiana</option>
+                            <option value="ME">Maine</option>
+                            <option value="MD">Maryland</option>
+                            <option value="MA">Massachusetts</option>
+                            <option value="MI">Michigan</option>
+                            <option value="MN">Minnesota</option>
+                            <option value="MS">Mississippi</option>
+                            <option value="MO">Missouri</option>
+                            <option value="MT">Montana</option>
+                            <option value="NE">Nebraska</option>
+                            <option value="NV">Nevada</option>
+                            <option value="NH">New Hampshire</option>
+                            <option value="NJ">New Jersey</option>
+                            <option value="NM">New Mexico</option>
+                            <option value="NY">New York</option>
+                            <option value="NC">North Carolina</option>
+                            <option value="ND">North Dakota</option>
+                            <option value="OH">Ohio</option>
+                            <option value="OK">Oklahoma</option>
+                            <option value="OR">Oregon</option>
+                            <option value="PA">Pennsylvania</option>
+                            <option value="RI">Rhode Island</option>
+                            <option value="SC">South Carolina</option>
+                            <option value="SD">South Dakota</option>
+                            <option value="TN">Tennessee</option>
 
-                        <option value="UT">Utah</option>
-                        <option value="VT">Vermont</option>
-                        <option value="VA">Virginia</option>
-                        <option value="WA">Washington</option>
-                        <option value="WV">West Virginia</option>
-                        <option value="WI">Wisconsin</option>
-                        <option value="WY">Wyoming</option>
-                    </select>
-                    <input
-                        type="text"
-                        name="zip"
-                        placeholder="Zip Code"
-                        required
-                    ></input>
+                            <option value="UT">Utah</option>
+                            <option value="VT">Vermont</option>
+                            <option value="VA">Virginia</option>
+                            <option value="WA">Washington</option>
+                            <option value="WV">West Virginia</option>
+                            <option value="WI">Wisconsin</option>
+                            <option value="WY">Wyoming</option>
+                        </select>
+                        <input
+                            type="text"
+                            name="zip"
+                            placeholder="Zip Code"
+                            required
+                        ></input>
+                    </section>
                     <section className="checkboxes">
                         <div>
                             <label className="toggle-switch-label" for="toggleSwitch">
-                                ATMS only?
-                                    </label>
-                            <input type="checkbox" name="locType" id="id" />
+                                ATMS only? <input type="checkbox" name="locType" id="id" />
+                            </label>
                         </div>
                         <div>
                             <label className="toggle-switch-label" for="toggleSwitch">
-                                Open 24hrs?
-                                    </label>
-                            <input type="checkbox" name="hr" id="id" />
+                                Open 24hrs? <input type="checkbox" name="hr" id="id" />
+                            </label>
                         </div>
                     </section>
                     <button
@@ -289,36 +288,52 @@ const Map = ({ title, center, zoom }) => {
     }
     return (
         <section className={wrapperClasses}>
-            <section className="form">
+            <section className="title">
                 <h1>
                     {title}
                 </h1>
+            </section>
+            <section className="form">
                 <Form />
-                <section className="results">
-                    <ul>
-                        {submitting ? 
-                            locations.map(() => (
-                                <li ref={hoverRef}>
-                                    <Skeleton />
-                                    <Skeleton count={2} />
-                                </li>
-                            ))
-                        :
-                            locations.map((place) => (
-                                <li ref={hoverRef}>
-                                    <h5>{place.institutionName}</h5>
-                                    <a href={`https://google.com/maps/place/${place.address},+${place.city},+${place.state}+${place.zip}`}>
-                                        <p>{place.address}</p>
-                                        <p>{place.city} {place.state}, {place.zip}</p>
-                                    </a>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                </section>
             </section>
             <section className="map">
                 <MapResults />
+            </section>
+            <section className="results">
+                <ul>
+                    {submitting ?
+                        locations.map(() => (
+                            <li>
+                                <Skeleton />
+                                <Skeleton count={2} />
+                            </li>
+                        ))
+                        :
+                        locations.map((place) => (
+                            <li>
+                                <section className="list-info">
+                                    <section className="list-pin">
+                                        <Pin />
+                                    </section>
+                                    <section className="text">
+                                        <h5>{place.institutionName}</h5>
+                                        <p>{place.address}</p>
+                                        <p>{place.city} {place.state}, {place.zip}</p>
+                                        <p><b>{place.locatorType === 'A' ? 'ATM' : 'Branch and ATM'}</b></p>
+                                    </section>
+                                </section>
+                                <section className="list-directions">
+                                    <p><b>Distance:</b> {place.distance} miles away</p>
+                                    <p>{place.city} {place.state}, {place.zip}</p>
+                                    <a href={`https://google.com/maps/place/${place.address},+${place.city},+${place.state}+${place.zip}`}>
+                                        Directions
+                                    </a>
+                                    
+                                </section>
+                            </li>
+                        ))
+                    }
+                </ul>
             </section>
         </section>
     )
@@ -328,7 +343,6 @@ Map.propTypes = {
     title: PropTypes.string,
     center: PropTypes.object,
     zoom: PropTypes.number,
-
 }
 
 export default Map
