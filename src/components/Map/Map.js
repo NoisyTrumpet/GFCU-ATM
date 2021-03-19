1cb70c44be79b4ffe37732acd1c9fb7d14b686d6
alexandrafossum <fossum.alexandra@gmail.com>
2021-03-19 14:02:24 -0500

    simplified search form to take just address and zipcode, fixed a few minor spelling errors in addresses

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -131,12 +131,12 @@ const Map = ({ title, center, zoom }) => {
                 data[key] = value
             }
 
-            const cityCheck = () => {
-                if (data.city) {
-                    return `&city=${data.city}`
-                }
-                return ''
-            }
+            // const cityCheck = () => {
+            //     if (data.city) {
+            //         return `&city=${data.city}`
+            //     }
+            //     return ''
+            // }
             const stateCheck = () => {
                 if (data.state && data.state !== 'Select') {
                     return `&state=${data.state}`
@@ -152,7 +152,7 @@ const Map = ({ title, center, zoom }) => {
             }
 
 
-            const request = `ZipCode=${data.zip}${cityCheck()}${stateCheck()}${addressCheck()}${`&locatortype=${data.locType === 'on' ? 'A' : 'AS'}`}${data.hr === 'on' ? `&open24hours=Yes` : ''}`
+            const request = `ZipCode=${data.zip}${stateCheck()}${addressCheck()}${`&locatortype=${data.locType === 'on' ? 'A' : 'AS'}`}${data.hr === 'on' ? `&open24hours=Yes` : ''}`
 
             getLocations(request)
         }
@@ -169,81 +169,16 @@ const Map = ({ title, center, zoom }) => {
                             placeholder="Address"
                         ></input>
                         <input
-                            className='input city-input'
-                            type="text"
-                            name="city"
-                            placeholder="City"
-                        ></input>
-                    </section>
-                    <section className="checkboxes">
-                        <select
-                            name="state"
-                            placeholder="State"
-                            className='input state'
-                        >
-                            <option value="Select" defaultValue="TX" disabled="">
-                                State
-              </option>
-                            <option value="TX">Texas</option>
-                            <option value="AL">Alabama</option>
-                            <option value="AK">Alaska</option>
-                            <option value="AZ">Arizona</option>
-                            <option value="AR">Arkansas</option>
-                            <option value="CA">California</option>
-                            <option value="CO">Colorado</option>
-                            <option value="CT">Connecticut</option>
-                            <option value="DE">Delaware</option>
-                            <option value="DC">District Of Columbia</option>
-                            <option value="FL">Florida</option>
-                            <option value="GA">Georgia</option>
-                            <option value="HI">Hawaii</option>
-                            <option value="ID">Idaho</option>
-                            <option value="IL">Illinois</option>
-                            <option value="IN">Indiana</option>
-                            <option value="IA">Iowa</option>
-                            <option value="KS">Kansas</option>
-                            <option value="KY">Kentucky</option>
-                            <option value="LA">Louisiana</option>
-                            <option value="ME">Maine</option>
-                            <option value="MD">Maryland</option>
-                            <option value="MA">Massachusetts</option>
-                            <option value="MI">Michigan</option>
-                            <option value="MN">Minnesota</option>
-                            <option value="MS">Mississippi</option>
-                            <option value="MO">Missouri</option>
-                            <option value="MT">Montana</option>
-                            <option value="NE">Nebraska</option>
-                            <option value="NV">Nevada</option>
-                            <option value="NH">New Hampshire</option>
-                            <option value="NJ">New Jersey</option>
-                            <option value="NM">New Mexico</option>
-                            <option value="NY">New York</option>
-                            <option value="NC">North Carolina</option>
-                            <option value="ND">North Dakota</option>
-                            <option value="OH">Ohio</option>
-                            <option value="OK">Oklahoma</option>
-                            <option value="OR">Oregon</option>
-                            <option value="PA">Pennsylvania</option>
-                            <option value="RI">Rhode Island</option>
-                            <option value="SC">South Carolina</option>
-                            <option value="SD">South Dakota</option>
-                            <option value="TN">Tennessee</option>
-
-                            <option value="UT">Utah</option>
-                            <option value="VT">Vermont</option>
-                            <option value="VA">Virginia</option>
-                            <option value="WA">Washington</option>
-                            <option value="WV">West Virginia</option>
-                            <option value="WI">Wisconsin</option>
-                            <option value="WY">Wyoming</option>
-                        </select>
-                        <input
                             type="text"
                             name="zip"
                             placeholder="Zip Code*"
                             required
                         ></input>
+
                     </section>
+                    {/* <section className="checkboxes">
+
+                    </section> */}
                     <section className="checkboxes">
                         <div>
                             <label className="toggle-switch-label" for="toggleSwitch">
